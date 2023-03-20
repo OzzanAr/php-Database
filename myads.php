0ec14692d38c1bebd202aa934e8947ef12968976
OzzanAr <ozan.arif@student.um.si>
2023-03-20 17:25:21 +0100

    Added myads page and basic html elements with Bootstrap styling.

---
+++ myads.php
@@ -0,0 +1,26 @@
+<?php
+include_once('header.php');
+
+
+
+
+
+
+?>
+<div class="container p-5 my-5 border"">
+	<div class="ad">
+		<h3 style="color:blue;"></h3>
+		<h5></h5>
+		<hr>
+		<p></p>
+
+		<img class="rounded"><br><br>
+		
+		<a><button class="btn btn-primary">Edit</button></a>
+		<a ><button class="btn btn-danger">Delete</button></a></br>
+    </div>
+</div>
+<?php
+
+include_once('footer.php');
+?>
