94f293e7e72a38d6d4775533323af31cb7c2fbad
OzzanAr <ozan.arif@student.um.si>
2023-03-20 11:28:50 +0100

    Function added to check submitted image file and added some basic variables to the publish function.

--- publish.php
+++ publish.php
@@ -3,6 +3,73 @@ include_once("header.php");
 
 $error = "";
 
+function publish($title, $desc, $category ,$img){
+	global $conn;
+	$title = mysqli_real_escape_string($conn, $title);
+	$desc = mysqli_real_escape_string($conn, $desc);
+	$user_id = $_SESSION["USER_ID"];
+
+
+	$path = imageCheck($img);
+	$path = mysqli_real_escape_string($conn, $path);
+
+	$unix_time = time();
+	$sql_timestamp = date('Y-m-d H:i:s', $unix_time);
+	$sub_time = date('Y-m-d H:i:s', strtotime($sql_timestamp . ' +1 hour')); // Add one hour to timestamp
+
+}
+
+function imageCheck($img){
+	$target_dir = "uploads/";
+	$target_file = $target_dir . basename($img["name"]);
+	$uploadOk = 1;
+	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+	$path=$target_file;
+
+	$imageError = "";
+	// Check if image file is a actual image or fake image
+	$check = getimagesize($img["tmp_name"]);
+	if($check !== false) {
+		$uploadOk = 1;
+	} else {
+		$imageError = "File is not an image.";
+		$uploadOk = 0;
+	}
+
+	// Check if file already exists
+	if (file_exists($target_file)) {
+		$imageError = "Sorry, file already exists.";
+		$uploadOk = 0;
+	}
+
+	// Check file size
+	if ($img["size"] > 500000) {
+		$imageError = "Sorry, your file is too large.";
+		$uploadOk = 0;
+	}
+
+	// Allow certain file formats
+	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+		$imageError = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+		$uploadOk = 0;
+	}
+
+	// Check if $uploadOk is set to 0 by an error
+	if ($uploadOk == 0) {
+		$error = "Sorry, your file was not uploaded.";	
+	} 
+	// if everything is ok, try to upload file
+	else {
+		if (move_uploaded_file($img["tmp_name"], $target_file)) {
+			return $path = $target_file;
+		} else {
+			$error = "Sorry, there was an error uploading your file.";
+		}
+	}
+}
+
+
 function getCategories(){
    global $conn;
 	$query = "SELECT name FROM category";
