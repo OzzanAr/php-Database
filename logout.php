29b28dd1ad31f8a065f425ffd7f859d3e374f792
OzzanAr <ozan.arif@student.um.si>
2023-03-20 01:01:19 +0100

    Added logging out of users accout, which resets the session and its variables.

--- logout.php
+++ logout.php
@@ -1,5 +1,8 @@
 <?php
-include_once('header.php');
-
-include_once('footer.php');
+// First loading the session then clearing the variables
+// destroying the session then redirecting the user to index.php
+session_start();
+session_unset();
+session_destroy();
+header("Location: index.php");
 ?>
