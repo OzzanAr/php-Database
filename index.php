0f194ffb1767357f2c9501f9c429e0411e76c189
OzzanAr <ozan.arif@student.um.si>
2023-03-20 10:59:14 +0100

    Added a function for reciving categories from the new table in the database and dispaying all of the categories.

--- index.php
+++ index.php
@@ -5,13 +5,32 @@ include_once('header.php');
 function get_ads(){
 	global $conn;
     // Ordering by date in descending order
-	$query = "SELECT ads.*, category.name as category_name FROM ads INNER JOIN category ON ads.category_id = category.id ORDER BY submission_time DESC";
+	$query = "SELECT ads.* FROM ads ORDER BY submission_time DESC";
+
 	$res = $conn->query($query);
 	$ads = array();
 	while($ad = $res->fetch_object()){
 		array_push($ads, $ad);
 	}
 	return $ads;
+}
+
+// Function to get the catogires from the ads_category table
+function get_categories($ad){
+	global $conn;
+
+	$currentAdID = $ad->id;
+
+	$query = "SELECT category.name as category_name FROM category
+	INNER JOIN ads_category ON $currentAdID = ads_category.ads_id
+	INNER JOIN ads ON category.id = ads_category.category_id";
+	
+	$res = $conn->query($query);
+	$categories = array();
+	while($category = $res->fetch_object()){
+		array_push($categories, $category);
+	}
+	return $categories;
 }
 
 // Reading the ads from the database
@@ -25,7 +44,16 @@ foreach($ads as $ad){
 	<div class="container p-5 my-5 border">
 		<div class="ad" id="cl">
 			<a href="ad.php?user_id=<?php echo $ad->id;?>" style="text-decoration: none"><h2><?php echo $ad->title;?></h2></a>
-			<h5><?php echo $ad->category_name;?></h5>
+			
+			<?php
+			$categories = get_categories($ad);
+			foreach($categories as $category){
+				?>	
+				<h5><?php echo $category->category_name;?></h5>
+				<?php
+			}
+			?>
+
 			<img class="rounded" src="<?php echo $ad->image ?>">
 		</div>
 	</div>
